9141d4c889abee06ecda08b764fbb92bf58c7632
Billy Ramos <billyrramos@gmail.com>
2015-10-04 10:20:15 -0700

    Add login view

---
+++ App/Login.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var React = require('react-native');
+var {
+  Text,
+} = React;
+
+var Login = React.createClass({
+    render: function () {
+        return (
+            <Text>Hello</Text>
+        );
+    }
+});
+
+module.exports = Login;
