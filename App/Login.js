db83a7150368cfbc2f8e2eea344b4bc5bca0315d
Billy Ramos <billyrramos@gmail.com>
2015-10-04 16:54:59 -0700

    Add image to login view

--- App/Login.js
+++ App/Login.js
@@ -4,13 +4,16 @@ var React = require('react-native');
 var {
   Text,
   StyleSheet,
-  View
+  View,
+  Image,
 } = React;
 
 var Login = React.createClass({
     render: function () {
         return (
-            <View style={styles.container}></View>
+            <View style={styles.container}>
+                <Image style={styles.logo} source={require('image!octocat')} />
+            </View>
         );
     }
 });
@@ -18,7 +21,13 @@ var Login = React.createClass({
 var styles = StyleSheet.create({
     container: {
         backgroundColor: '#f5fcff',
-        flex: 1
+        flex: 1,
+        paddingTop: 42,
+        alignItems: 'center'
+    },
+    logo: {
+        width: 66,
+        height: 55
     }
 })
 
